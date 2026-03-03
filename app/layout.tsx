1fca5f3d01b779b07f2dc344cba85d9d0f444cf7
Oussama <abboutoussama1998@gmail.com>
2026-03-03 02:13:44 -0500

    Ajouter les fichiers

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,11 @@ import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
-export const metadata = {
-  title: "Covoiturage",
-  description: "Plateforme de covoiturage",
-};
-
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="fr">
       <body>
